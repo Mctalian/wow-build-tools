02623d85b07517b8400592dabf66b34c209b0392
Rob Anderson <Mctalian@users.noreply.github.com>
2025-02-10 19:19:14 -0700

    fix: workaround contexts issue

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package main
 
 import (
+	"os"
+
 	"github.com/McTalian/wow-build-tools/cmd"
 	"github.com/McTalian/wow-build-tools/internal/logger"
 	"github.com/McTalian/wow-build-tools/internal/update"
@@ -11,6 +13,10 @@ import (
 
 func main() {
 	logger.InitLogger()
-	update.ConfirmAndSelfUpdate()
+	if os.Getenv("CI") == "true" {
+		logger.Verbose("Running in CI environment, no automatic updates will be performed")
+	} else {
+		update.ConfirmAndSelfUpdate()
+	}
 	cmd.Execute()
 }
