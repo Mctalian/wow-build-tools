b3961959e75d130f0e07803b780091a9535c53f8
Rob Anderson <Mctalian@users.noreply.github.com>
2025-02-11 18:30:05 -0700

    adjust how game versions arg is handled

--- internal/cliflags/cliflags.go
+++ internal/cliflags/cliflags.go
@@ -26,6 +26,7 @@ var CurseId string
 var WowiId string
 var WagoId string
 var GameVersion string
+var GameVerList []string
 var PkgmetaFile string
 var NameTemplate string
 
@@ -33,36 +34,81 @@ var UploadInput string
 var UploadLabel string
 var UploadInterfaceVersions []int
 
-func normalizeGameVersion() error {
-	orig := strings.ToLower(GameVersion)
+func parseGameVersionSegment(version string) (string, error) {
+	orig := strings.ToLower(version)
 	switch strings.ToLower(orig) {
 	case "retail", "classic", "bcc", "wrath", "cata":
-		GameVersion = orig
+		return orig, nil
 	case "mainline":
-		GameVersion = "retail"
+		return "retail", nil
 	default:
 		segments := strings.Split(orig, ".")
 		if len(segments) < 3 {
-			return fmt.Errorf("Invalid argument for game version: %s", orig)
+			return "", fmt.Errorf("Invalid argument for game version: %s", orig)
 		}
 		major, err := strconv.Atoi(segments[0])
 		if err != nil {
 			logger.Error("%v", err)
-			return fmt.Errorf("Invalid argument for game version: %s", orig)
+			return "", fmt.Errorf("Invalid argument for game version: %s", orig)
 		}
+		minor, err := strconv.Atoi(segments[1])
+		if err != nil {
+			logger.Error("%v", err)
+			return "", fmt.Errorf("Invalid argument for game version: %s", orig)
+		}
+		patch, err := strconv.Atoi(segments[2])
+		if err != nil {
+			logger.Error("%v", err)
+			return "", fmt.Errorf("Invalid argument for game version: %s", orig)
+		}
+
+		GameVerList = append(GameVerList, fmt.Sprintf("%d.%d.%d", major, minor, patch))
 
 		switch major {
 		case 1:
-			GameVersion = "classic"
+			return "classic", nil
 		case 2:
-			GameVersion = "bcc"
+			return "bcc", nil
 		case 3:
-			GameVersion = "wrath"
+			return "wrath", nil
 		case 4:
-			GameVersion = "cata"
+			return "cata", nil
 		default:
-			GameVersion = "retail"
+			return "retail", nil
 		}
+	}
+}
+
+func normalizeGameVersion() error {
+	if GameVersion == "" {
+		return nil
+	}
+
+	var versions []string
+	if strings.Contains(GameVersion, ",") {
+		// If it contains a comma, split it into multiple versions
+		v := strings.Split(GameVersion, ",")
+		for _, version := range v {
+			versions = append(versions, strings.TrimSpace(version))
+		}
+
+		for _, version := range versions {
+			_, err := parseGameVersionSegment(version)
+			if err != nil {
+				return err
+			}
+		}
+	} else {
+		// Only one version specified
+		g, err := parseGameVersionSegment(GameVersion)
+		if err != nil {
+			return err
+		}
+		GameVersion = g
+	}
+
+	if len(GameVerList) > 0 {
+		GameVersion = ""
 	}
 
 	return nil
